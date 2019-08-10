436651ee2d5aa9fe271b6e09027b44223839951e
lisz1012 <lisz1012@163.com>
2019-08-09 23:35:41 -0700

    Init

---
+++ src/main/java/com/lisz/SpringBootMyBatisApplication.java
@@ -0,0 +1,13 @@
+package com.lisz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootMyBatisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootMyBatisApplication.class, args);
+	}
+
+}
