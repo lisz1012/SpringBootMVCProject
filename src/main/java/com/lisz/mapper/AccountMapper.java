99df8413887deec074b4a8e313cdfb81c77aaae1
lisz1012 <lisz1012@163.com>
2019-08-10 20:40:33 -0700

    Use the annotations to link the SQL and method and use logging.level.com.lisz.mapper=debug for log printing

--- src/main/java/com/lisz/mapper/AccountMapper.java
+++ src/main/java/com/lisz/mapper/AccountMapper.java
@@ -3,6 +3,7 @@ package com.lisz.mapper;
 import java.util.List;
 
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Select;
 
 // MyBatis-Plus中ActiveRecord的思想是：new User().setName().setLocation().save()直接进数据库
 // 数据就绑定在对象上，一个对象就对应的一个数据。每个Entity类都得到增强，每个实体类增加了CRUD方法
@@ -10,6 +11,7 @@ import org.apache.ibatis.annotations.Mapper;
 public interface AccountMapper {
 
 	// 在application.properties.xml中去找sql的文件的位置，然后找到sql，然后通过反射或者动态代理，拿sql并实现类
+	@Select("select * from account")
 	List<Account> findAll();
 
 }
