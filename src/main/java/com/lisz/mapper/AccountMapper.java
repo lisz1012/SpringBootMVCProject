f67f6f3a9d1c9e976e91c61a9f5b7c188484887e
lisz1012 <lisz1012@163.com>
2019-08-17 23:09:44 -0700

    Add notes

--- src/main/java/com/lisz/mapper/AccountMapper.java
+++ src/main/java/com/lisz/mapper/AccountMapper.java
@@ -9,6 +9,6 @@ import org.springframework.stereotype.Repository;
  * AccountMapper继承基类
  */
 @Repository
-@Mapper
+@Mapper //这里注意要加上这个注解
 public interface AccountMapper extends MyBatisBaseDao<Account, Integer, AccountExample> {
 }
