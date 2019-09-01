a090136ea1374eec83518ebc95d8654c8f85edbc
lisz1012 <lisz1012@163.com>
2019-08-31 23:43:55 -0700

    Rename the starter class to generate the corret name of the jar file

--- src/main/java/com/lisz/SpringBootMVCProjectApplication.java
+++ src/main/java/com/lisz/SpringBootMVCProjectApplication.java
@@ -6,10 +6,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 //@MapperScan("com.lisz.mapper")//Spring启动的时候要注册一下，否则通过配置文件从容器中拿的时候会找不到，启动的时候就报错。或者在实体类脑袋上面写@Mapper
-public class SpringBootMyBatisApplication {
+public class SpringBootMVCProjectApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SpringBootMyBatisApplication.class, args);
+		SpringApplication.run(SpringBootMVCProjectApplication.class, args);
 	}
 
 }
