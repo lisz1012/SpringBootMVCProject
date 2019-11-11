97a17b265bd825a7320330ad4d52dc0de5171950
lisz1012 <lisz1012@163.com>
2019-11-10 21:26:17 -0800

    Add notes

--- src/main/java/com/lisz/entity/SysConfig.java
+++ src/main/java/com/lisz/entity/SysConfig.java
@@ -10,7 +10,7 @@ import org.springframework.stereotype.Component;
  */
 @Component //纳入spring 管理
 public class SysConfig {
-	@Value("${system.name}") //类似el表达式，当前指定的properties文件，如：spring.profiles.active=dev 或者没指定时默认的application.properties文件中没有system.name的话springboot会启动失败
+	@Value("${system.name}") //SpEL表达式，当前指定的properties文件，如：spring.profiles.active=dev 或者没指定时默认的application.properties文件中没有system.name的话springboot会启动失败
 	private String name;     //直接从命令行参数中取也能拿得到：java -jar SpringBootMVCProjectApplication-0.0.1-SNAPSHOT.jar --spring.profiles.active=dev --system.name=Google
 
 	public String getName() {
